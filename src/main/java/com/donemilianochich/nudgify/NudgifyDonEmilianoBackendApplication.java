d34dfe319dea1fd8334e57b1a5b02181bf18cf94
kfoq7 <frbrizoi@gmail.com>
2025-11-13 19:18:43 -0500

    feat(*): init project

---
+++ src/main/java/com/donemilianochich/nudgify/NudgifyDonEmilianoBackendApplication.java
@@ -0,0 +1,14 @@
+package com.donemilianochich.nudgify;
+
+import org.springframework.aop.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NudgifyDonEmilianoBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NudgifyDonEmilianoBackendApplication.class, args);
+  }
+
+}
